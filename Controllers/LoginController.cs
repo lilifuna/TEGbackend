032bd2612bd771ae2eed7cde04c1029516d93a38
maciejSamerdak <sam.maciej@interia.pl>
2018-05-09 22:18:21 +0200

    Tak na start.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEG.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+	{
+
+	}
+}
